0680bb971a84cb6a384e7a0e4d87b0afff0e811a
gliu22 <justdoitken@gmail.com>
2020-07-19 22:09:21 -0500

    timmers push
    1. timmers and interval

--- NodeJs/project1/questions.js
+++ NodeJs/project1/questions.js
@@ -1,6 +1,14 @@
 // process.stdout.write("Hello ");
 // process.stdout.write("World \n\n\n");
 
+// Although using const to define functions seems like a hack, but it comes with some great advantages that make it superior (in my opinion)
+
+// 1. It makes the function immutable, so you don't have to worry about that function being changed by some other piece of code.
+
+// 2. You can use fat arrow syntax, which is shorter & cleaner.
+
+// 3. Using arrow functions takes care of this binding for you.
+
 const questions = [
     "What is your name?",
     "What would you rather be doing?",
---
+++ NodeJs/project1/times.js
@@ -0,0 +1,25 @@
+const waitTime = 5000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () =>{
+    currentTime += waitInterval;
+    const p = Math.floor((currentTime / waitTime) *100);
+    //console.log(`Waiting ${currentTime/1000} seconds`);
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Waiting ... ${p}%`);
+};
+
+console.log(`Setting a ${waitTime / 1000} second delay`);
+
+const timerFinished = () => {
+    clearInterval(interval);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log("Done")
+};
+
+const interval  = setInterval(incTime, waitInterval);
+setTimeout(timerFinished, waitTime);
