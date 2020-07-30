76feab8d0aae846a80056fe94c63e6e3a91ae0c4
gliu22 <justdoitken@gmail.com>
2020-07-29 21:13:32 -0500

    event emitter push
    event emitter push

--- NodeJs/Node_Module/events.js
+++ NodeJs/Node_Module/events.js
@@ -1,3 +1,23 @@
 const event = require("events");
 
 const emitter = new event.EventEmitter();
+
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`);
+});
+
+// emitter.emit("customEvent", "Hello World", "Computer");
+
+// emitter.emit("customEvent", "That's pretty cool huh?","Ken");
+
+process.stdin.on("data", data =>{
+
+    const input = data.toString().trim();
+
+    if(input === 'exit'){
+        emitter.emit("customEvent", "Goodbye!","Process");
+        process.exit();
+    }
+
+    emitter.emit("customEvent", input,"Terminal");
+});
