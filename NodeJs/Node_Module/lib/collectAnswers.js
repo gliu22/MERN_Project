b3446a1ec30609f0b65e733cc70a9bed67709b33
gliu22 <justdoitken@gmail.com>
2020-07-29 21:37:33 -0500

    more event emiiter examples
    more event emiiter examples

--- NodeJs/Node_Module/lib/collectAnswers.js
+++ NodeJs/Node_Module/lib/collectAnswers.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const { Module } = require("module");
+const { EventEmitter } = require("events")
 
 const rl = readline.createInterface({
     input:process.stdin,
@@ -10,15 +10,19 @@ module.exports = (questions, done = f => f) =>{
     const answers = [];
     const [firstQuestion, secondQuestion, thirdQuestion] = questions;
     //done(answers);
-
+    const emitter = new EventEmitter();
     const questionAnswered = answer =>{
+        emitter.emit("answer",answer);
         answers.push(answer);
 
         if(answers.length < questions.length){
             rl.question(questions[answers.length], questionAnswered);
         }else{
+            emitter.emit("complete", answers);
             done(answers);
         }
     }
     rl.question(firstQuestion, questionAnswered);
+
+    return emitter;
 };
